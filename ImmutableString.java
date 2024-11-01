9f277dd8ae6100f6d90f5581b137a38c018484bb
bhoomika-pecheti <151710913+bhoomika-pecheti@users.noreply.github.com>
2024-11-01 16:01:07 +0530

    Create ImmutableString.java

---
+++ ImmutableString.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.lang.*;
+
+class ImmutableStringDemo
+{
+	public static void main(String args[])
+	{
+		String s="SASI";
+		s.concat("SITE");
+		System.out.println("After concatinating S1 and S2 : "+s);
+		s = "Bhoomika";
+		s=s.concat(Pecheti");
+		System.out.println("After concatinating S1 and S2: "+s);
+	}
+}
